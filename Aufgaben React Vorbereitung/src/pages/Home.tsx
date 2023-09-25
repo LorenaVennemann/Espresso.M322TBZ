e3cee563eb56ad2c63b2051a4c23c8b4764a9df4
Cameron Meile <cameron.meile@edu.tbz.ch~>
2023-09-25 11:04:05 +0200

    HTML + CSS Changes

--- Aufgaben React Vorbereitung/src/pages/Home.tsx
+++ Aufgaben React Vorbereitung/src/pages/Home.tsx
@@ -5,36 +5,52 @@ import "../Home.css";
 const Home: React.FC = () => {
   return (
     <div>
-      <nav className="navbar">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">AboutUs</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-        </ul>
-      </nav>
+      <header>
 
-      <h1 className="title">Espresso Oase</h1>
-      <p className="subtitle">by Cameron Meile & Lorena Vennemann</p>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+        </div>
 
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">AboutUs</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="TOSHOP">
+          <Link to="/shop">Shop</Link>
+        </div>
+
+      </header>
+
+      <div className="body">
+
+        <section className="Landing">
+          <div className="Home-Block">
+            <h1 className="title">Espresso Oase</h1>
+            <p className="subtitle">by Cameron Meile & Lorena Vennemann</p>
+            <Link to="/shop" className="shop-button">
+              Shop
+            </Link>
+          </div>
+        </section>
+
+        <section>
+
+        </section>
+
       </div>
-
-      <Link to="/shop" className="shop-button">
-        Shop
-      </Link>
     </div>
   );
 };
