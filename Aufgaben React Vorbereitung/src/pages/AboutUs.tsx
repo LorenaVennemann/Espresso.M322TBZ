d0f437705ed3ebb5ab5f9c381f62337de9dd7272
LorenaVennemann <113357105+LorenaVennemann@users.noreply.github.com>
2023-10-02 11:32:34 +0200

    aboutus

--- Aufgaben React Vorbereitung/src/pages/AboutUs.tsx
+++ Aufgaben React Vorbereitung/src/pages/AboutUs.tsx
@@ -6,40 +6,32 @@ import { Link } from 'react-router-dom';
 const AboutUs = () => {
   return (
     <div className={classes.app}>
-      <nav className={classes.navbar}>
-        <ul>
-          <li>
+      <header className={classes.header}>
+        <nav className={classes.navbar}>
+          <div className={classes.navbarName}>
             <Link to="/">Home</Link>
-          </li>
-          <li>
             <Link to="/about">About Us</Link>
-          </li>
-          <li>
             <Link to="/blog">Blog</Link>
-          </li>
-          <li>
             <Link to="/contact">Contact</Link>
-          </li>
-          <li>
             <Link to="/shop">Shop</Link>
-          </li>
-        </ul>
-      </nav>
+          </div>
+        </nav>
+      </header>
       <div className={classes.upper}>
         <h1 className={classes.title}>About Us</h1>
         <div className={classes.logoContainer}>
           <img src={logo} alt="Espresso Oasis" />
         </div>
         <div className={classes.content} style={{ margin: 'auto', maxWidth: '600px' }}>
-          <p style={{ textAlign: 'center', fontSize: '1.2em' }}>
+          <p style={{ textAlign: 'center', fontSize: '1.2em', color: 'black' }}>
             Willkommen bei Espresso Oasis – Ihrem Ziel für Espresso-Genuss. Wir sind leidenschaftliche Espresso-Enthusiasten und bieten Ihnen erstklassige Espresso-Produkte sowie unser Expertenwissen. Entdecken Sie bei uns die faszinierende Welt des Espressos und verfeinern Sie Ihr Kaffeeerlebnis. Vertrauen Sie auf unsere langjährige Leidenschaft und Erfahrung, um den perfekten Espresso-Genuss zu garantieren. Tauchen Sie ein in unsere Welt des aromatischen Kaffeegenusses!
           </p>
         </div>
       </div>
+      <div className={classes.lower}></div>
     </div>
   );
 };
 
 export default AboutUs;
-
 
