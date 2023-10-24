390948de4b7530c3f789539c8e1295c882f00e8c
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-10-24 09:41:42 +0200

    [CSS] Markup Change Shop

--- Aufgaben React Vorbereitung/src/pages/Shop.tsx
+++ Aufgaben React Vorbereitung/src/pages/Shop.tsx
@@ -3,15 +3,11 @@ import styles from '../Shop.module.css';
 import productImage from '../Coffee1.jpg';
 import productImage1 from '../Caffee2.jpg'
 import productImage2 from '../Coffee3.png'
-import classes from  "../Header.module.css"
-import "../Header.module.css"
+import classes from "../Root.module.css"
+import "../Root.module.css"
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
+
 const Shop = () => {
-   
-=======
-import "../Root.module.css";
->>>>>>> 5cd04d6f1f48f545cc06b603db3717cd55bcf9d1
 
   const products = [
     { id: '1', name: 'Lavazza-Coffee', price: '€12.85', image: productImage, origin: 'Italy', strength: 'Strong' },
@@ -20,16 +16,16 @@ import "../Root.module.css";
     { id: '1', name: 'Segafredo', price: '€12.85', image: productImage, origin: 'Italy', strength: 'Strong' },
     { id: '2', name: 'Saimaze', price: '€10.95', image: productImage1, origin: 'Spain', strength: 'Medium' },
     { id: '3', name: 'Dallmayr', price: '€9.99', image: productImage2, origin: 'Germany', strength: 'Light' }
-];
+  ];
 
-    const [filter, setFilter] = useState({ origin: '', strength: '' });
-    const filteredProducts = products.filter(product => 
-        (filter.origin === '' || product.origin === filter.origin) &&
-        (filter.strength === '' || product.strength === filter.strength)
-    );
+  const [filter, setFilter] = useState({ origin: '', strength: '' });
+  const filteredProducts = products.filter(product =>
+    (filter.origin === '' || product.origin === filter.origin) &&
+    (filter.strength === '' || product.strength === filter.strength)
+  );
 
-    return (
-      <div className={classes.div}>
+  return (
+    <div className={classes.div}>
       <header>
         <div className={`${classes["search-bar"]}`}>
           <input type="text" placeholder="Search..." />
@@ -52,32 +48,45 @@ import "../Root.module.css";
           </ul>
         </nav>
       </header>
-            <div className={styles.filter}>
-                <select onChange={(e) => setFilter({ ...filter, origin: e.target.value })}>
-                    <option value="">All origins</option>
-                    <option value="Italy">Italy</option>
-                    <option value="Spain">Spain</option>
-                    <option value="Germany">Germany</option>
-                </select>
-                <select onChange={(e) => setFilter({ ...filter, strength: e.target.value })}>
-                    <option value="">All strengths</option>
-                    <option value="Strong">Strong</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Light">Light</option>
-                </select>
-            </div>
-            <input type="text" placeholder="Search.." />
-            <div className={styles.container}>
-                {filteredProducts.map((product) => (
-                    <div key={product.id} className={styles.product}>
-                        <img src={product.image} alt={product.name} />
-                        <h2>{product.name}</h2>
-                        <p>{product.price}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+
+      <main>
+
+        <section>
+
+          <div className={styles.filter}>
+            <select onChange={(e) => setFilter({ ...filter, origin: e.target.value })}>
+              <option value="">All origins</option>
+              <option value="Italy">Italy</option>
+              <option value="Spain">Spain</option>
+              <option value="Germany">Germany</option>
+            </select>
+            <select onChange={(e) => setFilter({ ...filter, strength: e.target.value })}>
+              <option value="">All strengths</option>
+              <option value="Strong">Strong</option>
+              <option value="Medium">Medium</option>
+              <option value="Light">Light</option>
+            </select>
+          </div>
+          <input type="text" placeholder="Search.." />
+
+        </section>
+
+        <section>
+          <div className={styles.container}>
+            {filteredProducts.map((product) => (
+              <div key={product.id} className={styles.product}>
+                <img src={product.image} alt={product.name} />
+                <h2>{product.name}</h2>
+                <p>{product.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            ))}
+          </div>
+        </section>
+
+      </main>
+
+    </div>
+  );
 };
 export default Shop;
