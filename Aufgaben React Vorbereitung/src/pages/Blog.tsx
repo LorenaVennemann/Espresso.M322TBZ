79452b8cd2affd199522c16e51ef0ea132d169f6
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-10-31 09:31:52 +0100

    [CSS][HTML] Added Blog Classes

--- Aufgaben React Vorbereitung/src/pages/Blog.tsx
+++ Aufgaben React Vorbereitung/src/pages/Blog.tsx
@@ -45,32 +45,55 @@ const Blog: React.FC = () => {
 
       <main>
 
-        <section className={classes.image_full}>
-          <img src="https://cdn.pixabay.com/photo/2016/04/12/11/19/coffee-1324126_1280.jpg" alt="" />
+        <section className={classes.textbox_center}>
+          <h1>Welcome to our Blog!</h1>
         </section>
 
-        <section>
-          <h1>Willkommen auf unserem Blog!</h1>
-          <p>Hier finden Sie Neuigkeiten und Updates zu unserem Coffee-Shop sowie interessante Artikel rund um das Thema Kaffee.</p>
+        <section className={classes.blog_double}>
+          <div className={classes.left}>
+            <div className={classes.blog_card}>
+              <img src="https://cdn.pixabay.com/photo/2015/02/24/11/54/vienna-647328_1280.jpg" alt="" />
+              <div className={classes.text_box}>
+                <h1>"Brewing Journeys: A Caffeinated Chronicle"</h1>
+                <p>"Brewing Journeys: A Caffeinated Chronicle" is a captivating coffee blog that takes readers on a delightful exploration of the world of coffee. From the origins of different coffee beans to the art of brewing the perfect cup, this blog serves up a rich blend of coffee-related knowledge and inspiration.</p>
+              </div>
+            </div>
+            <div className={classes.blog_double}>
+              <div className={classes.blog_card}>
+                <img src="https://cdn.pixabay.com/photo/2015/06/24/01/15/coffee-819362_1280.jpg" alt="" />
+                <div className={classes.text_box}>
+                  <h1>"Bean Buzz: Savoring the Aromas, Stories, and Craft of Coffee"</h1>
+                  <p>"Bean Buzz" is a captivating coffee blog that takes you on a sensory journey through the world of coffee. Immerse yourself in the rich aromas, indulge in the captivating stories, and appreciate the meticulous craft that goes into every cup.</p>
+                </div>
+              </div>
+              <div className={classes.blog_card}>
+                <img src="https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_1280.jpg" alt="" />
+                <div className={classes.text_box}>
+                  <h1>"Coffee Chronicles: Tales from the Percolating World"</h1>
+                  <p>Uncover the hidden gems of coffee history as we delve into the origins and evolution of this beloved beverage. From ancient legends to modern-day innovations, we'll unravel the fascinating tales of discovery, trade, and cultural significance that have shaped coffee's journey throughout the ages.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={classes.right}>
+            <div className={classes.blog_card}>
+              <img src="https://cdn.pixabay.com/photo/2017/05/12/08/29/coffee-2306471_1280.jpg" alt="" />
+              <div className={classes.text_box}>
+                <h1>"Cup & Canvas: Blending Coffee and Creativity"</h1>
+                <p>"Cup & Canvas: Blending Coffee and Creativity" is an inspiring coffee blog that brings together two art forms – the exquisite world of coffee and the boundless realm of creativity. Step into a world where the aromatic brew fuels imagination, and each sip stirs the depths of your artistic spirit.</p>
+              </div>
+            </div>
+            <div className={classes.blog_card}>
+              <img src="https://cdn.pixabay.com/photo/2019/01/24/18/03/coffee-3953022_1280.jpg" alt="" />
+              <div className={classes.text_box}>
+                <h1>"Roast & Revel: Exploring the Bold Flavors of Coffee Culture"</h1>
+                <p>Indulge in the art of coffee roasting as we uncover the secrets behind achieving the perfect roast. Learn about the different roast profiles, from light and delicate to dark and robust, and discover how each roast level brings out distinct flavors and characteristics in the beans. Gain insights from expert roasters, explore roasting techniques, and even learn how to roast your own coffee at home.</p>
+              </div>
+            </div>
+          </div>
 
-          <h2>Der perfekte Kaffee für den Herbst</h2>
-
-          <p>Der Herbst ist da und mit ihm die kühleren Temperaturen und die wunderschönen Farben der Blätter. Was gibt es Schöneres, als an einem kühlen Herbstmorgen eine Tasse heißen Kaffee zu genießen?</p>
-
-          <p>Aber welcher Kaffee ist der beste für den Herbst? Hier sind einige unserer Favoriten:</p>
-
-          <ol>
-            <li><strong>Pumpkin Spice Latte:</strong> Dieser klassische Herbstkaffee ist ein Muss für alle, die den Geschmack von Kürbis und Gewürzen lieben. Probieren Sie es mit einem Schuss Vanillesirup für eine zusätzliche Süße.</li>
-
-            <li><strong>Caramel Macchiato:</strong> Dieser cremige Kaffee mit Karamellgeschmack ist perfekt für einen gemütlichen Herbstabend. Fügen Sie etwas Zimt hinzu, um ihm einen Hauch von Würze zu verleihen.</li>
-
-            <li><strong>Chai Latte:</strong> Dieser würzige Tee-Kaffee-Hybrid ist ein weiterer Favorit für den Herbst. Probieren Sie ihn mit einem Schuss Honig für eine natürliche Süße.</li>
-          </ol>
-
-          <p>Welcher Kaffee auch immer Sie wählen, vergessen Sie nicht, ihn mit einem leckeren Gebäck zu begleiten. Wir empfehlen unsere hausgemachten Muffins oder Scones.</p>
-
-          <p>Wir hoffen, dass Ihnen diese Empfehlungen gefallen haben. Besuchen Sie uns in unserem Coffee-Shop und probieren Sie unsere Herbstkaffees!</p>
         </section>
+
 
         <footer className={classes.footer}>
           <h1>Expresso Oase © 2023 ExpressoOase.com</h1>
