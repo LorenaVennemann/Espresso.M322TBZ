7b71e1e9389b5c7307203106db6c430d5a2cf949
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-10-31 13:49:37 +0100

    [CSS][HTML] Warenkorb Changes

--- Aufgaben React Vorbereitung/src/pages/Warenkorb.tsx
+++ Aufgaben React Vorbereitung/src/pages/Warenkorb.tsx
@@ -70,26 +70,36 @@ const Cart: React.FC = () => {
       </header>
 
       <main>
-        <h2>Shopping Cart</h2>
 
-        {cartItems.length === 0 ? (
-          <p>Your shopping cart is empty.</p>
-        ) : (
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.id}>
-                <img src={item.image} alt={item.name} />
-                <div>
-                  <h3>{item.name}</h3>
-                  <p>Price: €{item.price}</p>
-                  <button onClick={() => removeFromCart(item.id)}>Remove</button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
+        <section className={classes.textbox_start}>
+          <h2>Shopping Cart</h2>
+        </section>
 
-        <p>Total: €{getTotalPrice().toFixed(2)}</p>
+        <section className={classes.Checkout_container}>
+          {cartItems.length === 0 ? (
+            <p>Your shopping cart is empty.</p>
+          ) : (
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.id}>
+                  <div>
+                    <img src={item.image} alt={item.name} />
+                    <div>
+                      <h3>{item.name}</h3>
+                      <p>Price: €{item.price}</p>
+                      <br />
+                      <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+
+        <section>
+          <p>Total: €{getTotalPrice().toFixed(2)}</p>
+        </section>
 
       </main>
 
