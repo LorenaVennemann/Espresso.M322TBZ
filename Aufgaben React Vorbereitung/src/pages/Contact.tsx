fd4f920a86d5e0578323d9197dc060348e3c4e22
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-11-06 08:38:55 +0100

    [HTML][CSS] COntact Changes

--- Aufgaben React Vorbereitung/src/pages/Contact.tsx
+++ Aufgaben React Vorbereitung/src/pages/Contact.tsx
@@ -49,31 +49,57 @@ const Home: React.FC = () => {
           <p>Willkommen in der Expresso Oase, einem Ort, an dem Sie exquisiten Kaffeegenuss und eine Fülle von Aromen entdecken können. Unsere Leidenschaft für Kaffee spiegelt sich in jeder Tasse wider, die unsere erfahrenen Baristas mit Hingabe zubereiten. Von klassischen Kreationen bis hin zu innovativen Spezialitäten bieten wir Ihnen eine vielfältige Auswahl, die Ihre Geschmacksknospen verwöhnen wird.</p>
         </section>
 
-        <section></section>
 
-        <section className={classes.contat_form}>
-          <form action="#">
-            <div className={classes.contact_input}>
-            <label htmlFor="reason">Betreff</label>
-            <input type="text" name="reason" id="reason" />
+        <section className={classes.Formular}>
+          <div className={classes.LeftFOrm}>
+            <div>
+              <h1>Rufen Sie uns an</h1>
+              <div>
+                <a href="#">+41 019 12 12 13</a>
+                <a href="#">+41 019 12 12 13</a>
+              </div>
             </div>
-            <div className={classes.contact_input}>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
+            <div>
+              <h1>Unserer Standort</h1>
+              <div>
+                <a href="#">8633, Schaffhausen <br />Zürcherstrasse 17</a>
+              </div>
             </div>
-            <div className={classes.contact_input}>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <div>
+              <h1>Arbeitszeiten</h1>
+              <div>
+                <p>Mo-Fr : 6:00 - 19:00</p>
+                <p>Sa-So : 6:00 - 23:00</p>
+              </div>
             </div>
+          </div>
+          <div className={classes.FormRight}>
+            <form action="#">
+              <form action="#">
+                <div className={classes.contact_input}>
+                  <label htmlFor="reason">Betreff</label>
+                  <input type="text" name="reason" id="reason" />
+                </div>
+                <div className={classes.contact_input}>
+                  <label htmlFor="name">Name</label>
+                  <input type="text" name="name" id="name" />
+                </div>
+                <div className={classes.contact_input}>
+                  <label htmlFor="email">Email</label>
+                  <input type="email" name="email" id="email" />
+                </div>
 
-            <div className={classes.contact_input}>
-            <label htmlFor="text">Nachricht</label>
-            <input type="text" name="text" id="text" />
-            </div>
-            <div className={classes.contact_input}>
-            <input type="submit" name="submit" id="submit" />
-            </div>
-          </form>
+                <div className={classes.contact_input}>
+                  <label htmlFor="text">Nachricht</label>
+                  <input type="text" name="text" id="text" />
+                </div>
+                <br />
+                <div className={classes.contact_input}>
+                  <input type="submit" name="submit" id="submit" />
+                </div>
+              </form>
+            </form>
+          </div>
         </section>
 
         <footer className={classes.footer}>
